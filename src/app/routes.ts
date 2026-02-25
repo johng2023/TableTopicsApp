1a441c907025c4af28f02c3a8219634670b64c65
John G <john.guckian13@gmail.com>
2026-02-24 19:38:28 -0700

    feat: register /dashboard/:id route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import { Home } from "./screens/Home";
 import { Recording } from "./screens/Recording";
 import { History } from "./screens/History";
+import { Dashboard } from "./screens/Dashboard";
 
 export const router = createBrowserRouter([
   {
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
   {
     path: "/history",
     Component: History,
+  },
+  {
+    path: "/dashboard/:id",
+    Component: Dashboard,
   },
 ]);
